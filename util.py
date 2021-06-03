348a7baadc1640dadb610a642171bbf0c14bb696
HZI2 <hicham.zouarhi@3ds.com>
2021-06-03 10:13:51 +0200

    add browser_cookies3 to bypass google consent to cookies use

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import requests
 from xml.etree import ElementTree
 from model import News
+import browser_cookie3
+import json
 
 import constants
 
@@ -16,11 +18,15 @@ def get_news_feed_by_location(location):
     :return a list of news ( objects ):
     '''
     url = constants.GEO_URL + location
-    response = requests.get(url=url)
+    # print("getting browser cookies")
+    cookies = browser_cookie3.chrome(domain_name='.google.com')
+    # print("sending GET request")
+    response = requests.get(url=url, cookies=cookies)
     tree = ElementTree.fromstring(response.content)
     items = tree.findall(constants.ITEM_TAG)
     news_list = []
     for item in items:
+        print(item)
         news_list.add(get_news_from_item(item, location))
 
-    return news_list
+    return json.dumps(news_list)
