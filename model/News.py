978171a6c2aa8f30e55bbfca8e4886b4b3c5a874
HZI2 <hicham.zouarhi@3ds.com>
2021-06-03 10:54:28 +0200

    fix GET newsbygeo using ElementTree

--- model/News.py
+++ model/News.py
@@ -11,9 +11,18 @@ class News:
         self.description = None
 
     def hydrate(self, item, location):
-        self.title = item.get(constants.TITLE_TAG)
+        self.title = item.find(constants.TITLE_TAG).text
         self.location = location
-        self.link = item.get(constants.LINK_TAG)
-        self.publication_date = item.get(constants.PUBLICATION_DATE_TAG)
-        self.source = item.get(constants.SOURCE_TAG)
-        self.description = item.get(constants.DESCRIPTION_TAG)
+        self.link = item.find(constants.LINK_TAG).text
+        self.publication_date = item.find(constants.PUBLICATION_DATE_TAG).text
+        self.source = item.find(constants.SOURCE_TAG).text
+        self.description = item.find(constants.DESCRIPTION_TAG).text
+
+    def dump(self):
+        print("item ---")
+        print("title : " + self.title)
+        print("location : " + self.location)
+        print("link : " + self.link)
+        print("publication date : " + self.publication_date)
+        print("source : " + self.source)
+        print("description : " + self.description)
--- util.py
+++ util.py
@@ -3,11 +3,11 @@ from xml.etree import ElementTree
 from model import News
 import browser_cookie3
 import json
-
 import constants
 
+
 def get_news_from_item(item, location):
-    news = News()
+    news = News.News()
     news.hydrate(item, location)
     return news
 
@@ -18,15 +18,13 @@ def get_news_feed_by_location(location):
     :return a list of news ( objects ):
     '''
     url = constants.GEO_URL + location
-    # print("getting browser cookies")
     cookies = browser_cookie3.chrome(domain_name='.google.com')
-    # print("sending GET request")
     response = requests.get(url=url, cookies=cookies)
-    tree = ElementTree.fromstring(response.content)
-    items = tree.findall(constants.ITEM_TAG)
+    root = ElementTree.fromstring(response.content)
+    channel = root.find(constants.CHANNEL_TAG)
+    # items = channel.findall(constants.ITEM_TAG)
     news_list = []
-    for item in items:
-        print(item)
-        news_list.add(get_news_from_item(item, location))
+    for item in channel.iter(constants.ITEM_TAG):
+        news_list.append(get_news_from_item(item, location))
 
-    return json.dumps(news_list)
+    return json.dumps([ob.__dict__ for ob in news_list])
