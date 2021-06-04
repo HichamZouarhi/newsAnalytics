63f2537437fcc7d3be89a27afe6473cd13675733
Hicham Zouarhi <hicham.zouarhi@gmail.com>
2021-06-04 11:50:13 +0200

    add keywordsbygeo using nltk

--- home.py
+++ home.py
@@ -1,13 +1,26 @@
 from flask import Flask
-
 import util
+import json
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+
 @app.route('/newsbygeo/<string:location>')
 def get_news_by_geo(location):
-    return util.get_news_feed_by_location(location)
+    news_list = util.get_news_feed_by_location(location)
+    return json.dumps([ob.__dict__ for ob in news_list])
+
+
+@app.route('/keywordsbygeo/<string:location>')
+def get_key_words_by_geo(location):
+    news_list = util.get_news_feed_by_location(location)
+    key_words = util.get_most_used_words_by_location(news_list)
+    print(key_words)
+    return json.dumps(key_words)
+
--- util.py
+++ util.py
@@ -2,8 +2,10 @@ import requests
 from xml.etree import ElementTree
 from model import News
 import browser_cookie3
-import json
 import constants
+import nltk
+from string import punctuation
+from collections import Counter
 
 
 def get_news_from_item(item, location):
@@ -13,18 +15,31 @@ def get_news_from_item(item, location):
 
 
 def get_news_feed_by_location(location):
-    '''
+    """
     :param location:
     :return a list of news ( objects ):
-    '''
+    """
     url = constants.GEO_URL + location
     cookies = browser_cookie3.chrome(domain_name='.google.com')
     response = requests.get(url=url, cookies=cookies)
     root = ElementTree.fromstring(response.content)
     channel = root.find(constants.CHANNEL_TAG)
-    # items = channel.findall(constants.ITEM_TAG)
     news_list = []
     for item in channel.iter(constants.ITEM_TAG):
         news_list.append(get_news_from_item(item, location))
 
-    return json.dumps([ob.__dict__ for ob in news_list])
+    return news_list
+
+
+def get_most_used_words_by_location(news_list):
+    """
+    :param news_list: list of news (objects)
+    :return: str most used word, if more than one returns a list
+    """
+    stopwords = set(nltk.corpus.stopwords.words('english'))
+    key_words = Counter()
+    for news in news_list:
+        print(news.description)
+        key_words.update(w.lower().rstrip(punctuation) for w in news.description.split(" ") if w not in stopwords)
+
+    return dict((k, v) for k, v in key_words.most_common(10))
